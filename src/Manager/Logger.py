28619229da34be69c32e22426d909f26d9a1e8db
j1gsaw <j1gsaw@qq.com>
2025-02-17 21:11:17 +0800

    1. 测试文件从test.ipynb变成了test.py 2. 修复了无法读取config\config.json文件的bug 3. 修复了当type为Info时, 无法输出日志的bug

--- src/Manager/Logger.py
+++ src/Manager/Logger.py
@@ -5,11 +5,42 @@ import time
 
 current_path = 'd:\\Code\\Project\\AIPartner\\'
 
+ColorDic = {
+    'default' : '\033[0m',
+    'blod' : '\033[1m',
+    'underline' : '\033[4m',
+    'black' : '\033[30m',
+    'red' : '\033[31m',
+    'green' : '\033[32m',
+    'yellow' : '\033[33'
+}
+
 LogTypeColorDic = {}
+
+def get_color_utf8(
+    color : str, 
+    ) -> str:
+    """通过颜色获取其对应的UTF-8码
+    Args:
+        color (str): 颜色字符串
+    Returns:
+        str: 带有颜色的字符串
+    """
+    string = ColorDic.get(color)
+    if len(string) == 0:
+        return ColorDic['default']
+    return string
+def get_color_reset_utf8() -> str:
+    """获取重置颜色的UTF-8码
+    Returns:
+        str: 重置颜色的UTF-8码
+    """
+    return '\033[0m'
+    
+    
 
 class Logger:
     log_output_path = ''
-    
     
     def __init__(self):
         self.load_default_log_output_path()
@@ -38,23 +69,6 @@ class Logger:
             LogTypeColorDic['Warn'] = config['default-log-type-color']['Warn']
             LogTypeColorDic['Error'] = config['default-log-type-color']['Error']
     
-    def Str2Log(self, head : str, info : str) -> str:
-        """
-        字符串转日志格式 ==== [头部]年-月-日 时:分:秒 : info
-        Args:
-            head (str): 日志头部 
-            info (str): 详细信息
-        Returns:
-            str: 日志字符串
-        """
-        # 获取当前时间并格式化为 '年-月-日 时:分:秒'
-        formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        # 拼接时间与信息，格式为 "时间 : info"
-        log_message = f"[{head}]{formatted_time} : {info}"    
-        # 返回日志字符串
-        return log_message
-    
-        
     def log(self, info : str, ) -> None:                                         
         """
         记录日志
@@ -64,22 +78,34 @@ class Logger:
         with open(self.log_file_path, 'a') as f:
             f.write(info + '\n')
             
-    def print_log(self, log : str, type : str) -> None:
+    def print_log(self, type : str, head : str, info : str) -> None:
         """ 
         输出日志
         Args:
-            log (str): 要输出的日志字符串
             type (str): 日志类型 - |Info|Warn|Error|
+            head (str): 日志头部信息
+            info (str): 要输出的字符串
         """
-        print(f'{type} - ' + log)
+        # 获取当前时间并格式化为 '年-月-日 时:分:秒'
+        formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        # 拼接时间与信息，格式为 "时间 : info"
+        log_message = f" {type} - [{head}]{formatted_time} : {info}"    
         
-    def print_colored_log(self, log : str, type : str) -> None:
+        print(log_message)
+        
+    def print_colored_log(self, type : str, head : str, info : str) -> None:
         """ 
         根据日志的类型，输出不同颜色的日志 | 在config.json中配置
         Args:
-            log (str): 要输出的日志字符串
             type (str): 日志类型 - |Info|Warn|Error|
+            head (str): 日志头部信息
+            info (str): 要输出的字符串
         """
-        print()
+        # 获取当前时间并格式化为 '年-月-日 时:分:秒'
+        formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        # 拼接时间与信息，格式为 "时间 : info"
+        log_message = f" {type} - [{head}]{formatted_time} : {info}"
+            
+        print(get_color_utf8(LogTypeColorDic.get(type)) + log_message + get_color_reset_utf8())
         
         
